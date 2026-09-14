5236305eb0a4630a6dd918c133aa323884a57f83
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-09-14 19:11:12 +0200

    Update 013_verificacion_datos.py

--- Módulo 1 - Ejercicios/013_verificacion_datos.py
+++ Módulo 1 - Ejercicios/013_verificacion_datos.py
@@ -6,3 +6,24 @@ def comprobar_datos(nombre : str, edad : int, nivel_ingles : str,
     el nivel de inglés es 'MEDIO' o 'ALTO'
     y tiene auto (coche, moto, bici...)
     '''
+    EDAD_MINIMA = 18
+    EDAD_MAXIMA = 67
+    if edad<EDAD_MINIMA or edad>=EDAD_MAXIMA:
+        return False
+
+    # Versión Fernando
+    if nivel_ingles=='BAJO':
+        return False
+
+    # Versión Jose
+    # niveles_aceptados = ('MEDIO','ALTO')
+    # if nivel_ingles not in niveles_aceptados:
+    #     return False
+
+    if tiene_auto==False:
+        return False
+
+    return True
+
+resultado = comprobar_datos('Gema', 25, 'ALTO', True)
+print(resultado)
