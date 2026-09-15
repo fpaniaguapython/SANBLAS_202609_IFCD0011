8bcb209b1921fee190e445ab7c0a924124d99a98
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-09-15 20:58:28 +0200

    Update 022_movies_finder.py

--- Módulo 1 - Ejercicios/022_movies_finder.py
+++ Módulo 1 - Ejercicios/022_movies_finder.py
@@ -6,5 +6,8 @@ with open('movies.csv', newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         peliculas.append(row) # ['131231', 'Standby (2014)' , 'Comedy|Romance']
-        #peliculas.append([int(row[0]),row[1][:-7],int(row[1][-5:-1]),row[2]])
+        #peliculas.append((int(row[0]),row[1][:-7],int(row[1][-5:-1]),row[2]))
     del peliculas[0]
+
+# Pedir una palabra al usuario
+# Buscar las películas cuyo título contiene esa palabra
