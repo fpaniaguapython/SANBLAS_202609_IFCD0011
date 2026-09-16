2f9eb7dd01c21fdb28ca7db30d2591664c4de97d
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-09-16 20:31:33 +0200

    Update 023_comprension_listas.py

--- Módulo 1 - Ejercicios/023_comprension_listas.py
+++ Módulo 1 - Ejercicios/023_comprension_listas.py
@@ -1,5 +1,12 @@
 ingredientes = ['Harina', 'Leche', 'Huevo', 'Vino', 'Sal']
+
+# Versión tradicional
 mayusculas = []
 for ingrediente in ingredientes:
     mayusculas.append(ingrediente.upper())
-    
+
+print(mayusculas)
+
+# Comprensión de lista
+ingredientes_mayusculas = [ingrediente.upper() for ingrediente in ingredientes] 
+print(ingredientes_mayusculas)
