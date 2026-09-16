d575d6279b24d82b17e5f98854eac661826be47b
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-09-16 20:28:34 +0200

    Ejercicios

---
+++ Módulo 1 - Ejercicios/023_comprension_listas.py
@@ -0,0 +1,5 @@
+ingredientes = ['Harina', 'Leche', 'Huevo', 'Vino', 'Sal']
+mayusculas = []
+for ingrediente in ingredientes:
+    mayusculas.append(ingrediente.upper())
+    
---
+++ Módulo 1 - Ejercicios/024_paises_comprension_listas.py
@@ -0,0 +1,17 @@
+# Lista de países con (Nombre, Población, PIB en €, Superficie en km²)
+paises_eur = [
+    ("España", 48000000, 1472000000000, 505990),
+    ("México", 128500000, 1343200000000, 1964375),
+    ("Estados Unidos", 335000000, 25760000000000, 9833520),
+    ("China", 1410000000, 16560000000000, 9596961),
+    ("Japón", 124500000, 3864000000000, 377975),
+    ("Brasil", 215000000, 1978000000000, 8515767),
+    ("Alemania", 84400000, 4048000000000, 357022),
+    ("India", 1430000000, 3496000000000, 3287263),
+    ("Francia", 68000000, 2760000000000, 551695),
+    ("Argentina", 46200000, 588800000000, 2780400)
+]
+# Crear una nueva Lista en la que aparezcan:
+# Nombre del país
+# Densidad de Habitantes por km^2
+# PIB/habitante
