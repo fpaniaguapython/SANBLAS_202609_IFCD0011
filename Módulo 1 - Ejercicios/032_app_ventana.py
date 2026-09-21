d01192ac302eec457303c84e9d075e9d6072c020
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-09-21 18:54:09 +0200

    App ventanas

--- Módulo 1 - Ejercicios/032_app_ventana.py
+++ Módulo 1 - Ejercicios/032_app_ventana.py
@@ -1,7 +1,18 @@
-import tkinter as tk
+import tkinter as tk # Importación de tkinter
+import json
 
-main_window=tk.Tk()
-main_window.title('Principal')
+COLOR_FONDO = 'COLOR_FONDO'
+TITULO_VENTANA = 'TITULO_VENTANA'
+ANCHO_VENTANA = 'ANCHO_VENTANA'
+ALTO_VENTANA = 'ALTO_VENTANA'
+
+with open('config_ventana.json', mode='rt', encoding='utf-8') as fichero:
+    configuracion = json.load(fichero)
+
+main_window=tk.Tk() # Creación de un objeto de la clase Tk
+
+main_window.title(configuracion[TITULO_VENTANA])
 main_window.geometry('800x400')
-main_window.configure(bg='yellow')
-main_window.mainloop()
+main_window.configure(bg=configuracion[COLOR_FONDO])
+
+main_window.mainloop() # Pone en bucle infinito la aplicación
