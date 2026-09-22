b629968b8a34f429a0c09591a88bb1cf35e04851
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-09-22 16:22:26 +0200

    POO

--- Módulo 1 - Ejercicios/POO/01_clase_basica.py
+++ Módulo 1 - Ejercicios/POO/01_clase_basica.py
@@ -1,11 +1,31 @@
+# DECLARACIÓN DE LA CLASE
 class Motor:
+    # CONSTRUCTOR
     def __init__(self, nombre, potencia, cilindrada, combustible):
+        # ATRIBUTOS
         self.nombre = nombre
         self.potencia = potencia
         self.cilindrada = cilindrada
         self.combustible = combustible
+        self.arrancado = False
+        self.velocidad = 0
 
+    # MÉTODOS
+    def arrancar(self):
+        self.arrancado = True
+
+    def acelerar(self, incremento):
+        if self.arrancado==True:
+            # self.velocidad=self.velocidad+incremento        
+            self.velocidad+=incremento        
+
+# INSTANCIACIÓN --> CREACIÓN DE UNA INSTANCIA U OBJETO
 motor_v8 = Motor('MOTOR SUPER V8', 400, 1000, 'GASOLINA')
 motor_v16 = Motor('MOTOR SUPER V16', 600, 3000, 'GASOIL')
 
-print(motor_v8.__dict__)
+# INVOCAR AL MÉTODO arrancar
+motor_v8.arrancar()
+
+# Acelera el motor en 100km/h
+motor_v8.acelerar(100)
+print(motor_v8.velocidad)
