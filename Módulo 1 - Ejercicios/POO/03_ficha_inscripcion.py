b4a7f2ccc0334b60f8c56b81f984063535fb97ea
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-09-22 18:36:28 +0200

    Create 03_ficha_inscripcion.py

---
+++ Módulo 1 - Ejercicios/POO/03_ficha_inscripcion.py
@@ -0,0 +1,14 @@
+"""
+Crear un clase que recoja la ficha de inscripción en un gimnasio.
+Los atributos son:
+- Nombre.
+- Edad.
+- Número de teléfono.
+- Email.
+
+Crear los métodos siguiente:
+- Mostrar los datos de la ficha por pantalla.
+- Comprobar que todos los atributos tienen valro (no son None)
+- Escribir los datos de la ficha en un fichero de texto.
+- Escribir los datos de la ficha en un fichero json.
+"""
