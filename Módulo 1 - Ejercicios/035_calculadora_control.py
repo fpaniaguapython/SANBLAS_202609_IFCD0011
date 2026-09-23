664e646664cd0aeee6f789d3ac5b24ea5c7a5b73
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-09-23 17:36:10 +0200

    Update 035_calculadora_control.py

--- Módulo 1 - Ejercicios/035_calculadora_control.py
+++ Módulo 1 - Ejercicios/035_calculadora_control.py
@@ -7,3 +7,16 @@ Capturar y tratar los errores de conversión.
 Cuando se divide entre 0 se produce un 
 error. Hay que capturarlo y tratarlo.
 '''
+try:
+    numero_1 = int(input('Número 1:'))
+    numero_2 = int(input('Número 2:'))
+    suma = numero_1 + numero_2
+    print(f'La suma es {suma}')
+    resta = numero_1 - numero_2
+    print(f'La resta es {resta}')
+    division = numero_1 / numero_2
+    print(f'La division es {division}')
+except ValueError:
+    print('Has introducido mal los números')
+except ZeroDivisionError:
+    print('No se puede dividir entre 0')
